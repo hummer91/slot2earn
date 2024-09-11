52f0bd989509c7fe7121f70c9134aa7eee38c88e
snkrzarchive <thesnkrz@thesnkrz.com>
2024-09-11 13:52:56 +0900

    ver0.2.2 add folders, level_up_bonus auto claim

--- scripts/convert_paylines_csv_3X5.py
+++ scripts/convert_paylines_csv_3X5.py
@@ -34,8 +34,8 @@ def convert_paylines(input_file, output_file):
             writer.writerow([payline_id, ';'.join(positions)])
 
 # 파일 경로 설정
-input_file = 'csv/payline_original_3X5.csv'
-output_file = 'csv/paylines_3x5_1.csv'
+input_file = 'csv/paylines/payline_original_3X5.csv'
+output_file = 'csv/paylines/paylines_3x5.csv'
 
 # 변환 함수 호출
 convert_paylines(input_file, output_file)
